836cd4beb103b97226f0179ee90627224506012d
Konrad Kahnert <kkahnert49@gmail.com>
2022-01-10 13:19:12 -0600

    bullet is now destroyed after a set amount of bounces and added target

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bullet : MonoBehaviour
 {
     float xDestroyBound = 10f;
     float yDestroyBound = 6f;
+    int bounceCount = 0;
 
-    // Destroy this object if it goes beyond xDestroyBound or yDestroyBound
     void CheckToDestroy()
     {
+        // Destroy this object if it goes beyond xDestroyBound or yDestroyBound
         if ((transform.position.x >= xDestroyBound) || (transform.position.x <= -xDestroyBound) || (transform.position.y >= yDestroyBound) || (transform.position.y <= -yDestroyBound))
+        {
+            Destroy(gameObject);
+        }
+
+        if (bounceCount >= 4)
         {
             Destroy(gameObject);
         }
@@ -17,5 +24,21 @@ public class Bullet : MonoBehaviour
     private void Update()
     {
         CheckToDestroy();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.transform.tag == "Wall") || (collision.transform.tag == "Slope"))
+        {
+            bounceCount++;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Target")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }

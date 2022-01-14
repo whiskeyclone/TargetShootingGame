0978d7c62d3c9789e0aa5350a08d59ca0ca98d4d
Konrad Kahnert <kkahnert49@gmail.com>
2022-01-14 15:38:32 -0600

    fixed blinking walls scale issue

--- Assets/Scripts/BlinkingWall.cs
+++ Assets/Scripts/BlinkingWall.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BlinkingWall : MonoBehaviour
 {
     [SerializeField] int onState; // The state where this wall is active
+    Vector3 scale;
 
     private void Start()
     {
@@ -10,18 +11,23 @@ public class BlinkingWall : MonoBehaviour
         {
             Debug.LogError("Invalid on state!");
         }
+
+        scale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Enable/disable sprite and collider
         if (BlinkTimer.instance.GetState() == onState)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = scale;
+            GetComponent<BoxCollider2D>().enabled = true;
         }
         else
         {
             transform.localScale = new Vector3(0, 0, 0);
+            GetComponent<BoxCollider2D>().enabled = false;
         }
     }
 }
